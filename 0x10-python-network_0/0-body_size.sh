8cf7f70b060ba674ff78e7c7e13569106034c004
ShadowWalkerfb <87659323+ShadowWalkerfb@users.noreply.github.com>
2023-03-30 23:47:50 +0000

    commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,16 +1,3 @@
-
 #!/bin/bash
-
-url=$1
-
-
-response=$(curl -s -w '\n%{size_download}\n' "$url")
-
-
-size=$(echo "$response" | tail -1)
-
-
-echo "The size of the response body is $size bytes."
-
-
-
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
